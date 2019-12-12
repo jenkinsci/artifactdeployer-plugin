d4f705a50f4578234c233da1666641da483bde5b
Andrew M. Stoltz <andrew.stoltz@chrobinson.com>
2019-12-12 11:15:38 -0600

    Switch from org.jruby.ext.posix.POSIX to jnr.posix.POSIX

--- src/main/java/org/jenkinsci/plugins/artifactdeployer/service/PermissionsUtils.java
+++ src/main/java/org/jenkinsci/plugins/artifactdeployer/service/PermissionsUtils.java
@@ -1,20 +1,17 @@
 package org.jenkinsci.plugins.artifactdeployer.service;
 
 import java.io.File;
-import java.io.InputStream;
-import java.io.PrintStream;
 
-import org.jruby.ext.posix.POSIX;
-import org.jruby.ext.posix.POSIX.ERRORS;
-import org.jruby.ext.posix.POSIXFactory;
-import org.jruby.ext.posix.POSIXHandler;
+import jnr.posix.POSIX;
+import jnr.posix.POSIXFactory;
+import jnr.posix.util.DefaultPOSIXHandler;
 
 /**
  * Provides convenience methods for manipulating file permissions, as best as is achievable with the Java 6 API.
  */
 public class PermissionsUtils
 {
-    private static POSIX posix = POSIXFactory.getPOSIX(new PermissionsUtils.AntPOSIXHandler(), true);
+    private static POSIX posix = POSIXFactory.getPOSIX(new DefaultPOSIXHandler(), true);
 
     public static int getPermissions(File file)
     {
@@ -25,65 +22,4 @@ public class PermissionsUtils
     {
         posix.chmod(file.getAbsolutePath(), perms);
     }
-
-    /**
-     * Minimal POSIX handler for Ant tasks. There's scope for improvement here, like redirecting warnings through the Ant
-     * log, failing on errors, etc...
-     */
-    public static class AntPOSIXHandler implements POSIXHandler
-    {
-
-        public File getCurrentWorkingDirectory()
-        {
-            return new File(".");
-        }
-
-        public String[] getEnv()
-        {
-            return new String[]{};
-        }
-
-        public PrintStream getErrorStream()
-        {
-            return System.err;
-        }
-
-        public InputStream getInputStream()
-        {
-            return System.in;
-        }
-
-        public PrintStream getOutputStream()
-        {
-            return System.out;
-        }
-
-        public int getPID()
-        {
-            return 0;
-        }
-
-        public boolean isVerbose()
-        {
-            return false;
-        }
-
-        public void unimplementedError(String message)
-        {
-            throw new RuntimeException(message);
-        }
-
-        public void warn(WARNING_ID arg0, String arg1, Object... arg2)
-        {
-            System.err.println(arg0 + ": " + String.format(arg1, arg2));
-        }
-
-
-        public void error(ERRORS error, String extraData)
-        {
-            System.err.println(error + ": " + extraData);
-        }
-
-    }
-
 }
